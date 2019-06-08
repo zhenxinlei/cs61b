79bfd0f5b5f6738fb76dbfb542a042d272231861
ZhenxinLei <zhenxinlei@gmail.com>
2019-06-08 17:52:58 -0400

    save :

---
+++ src/Algorithm/DoubleLinkedNode.java
@@ -0,0 +1,19 @@
+package Algorithm;
+
+public class DoubleLinkedNode {
+	int val;
+	      DoubleLinkedNode left;
+	      DoubleLinkedNode right;
+	      DoubleLinkedNode(int x) { val = x; }
+	      
+	public DoubleLinkedNode generateBTree(){
+		DoubleLinkedNode root=new DoubleLinkedNode((int) Math.round((double)Math.random()*10));
+		int i=(int) Math.round((double)Math.random()*10);
+		
+		
+		
+		
+		return root;
+	}
+	 
+}
