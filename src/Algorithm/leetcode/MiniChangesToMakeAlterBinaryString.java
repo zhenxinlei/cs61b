836b0bd6a372c0fb8fa398694938b467bec80a7c
zlei <zhenxinlei@gmail.com>
2021-12-09 10:19:02 -0500

    save 2021/11/08

---
+++ src/Algorithm/leetcode/MiniChangesToMakeAlterBinaryString.java
@@ -0,0 +1,41 @@
+package Algorithm.leetcode;
+
+/**
+ * leetcode #1758
+ * change '0','1' only string to no two adjacent char are equal
+ *
+ * example:
+ *    0100 -> 0101  return 1 (step)
+ *    01 -> 01 return 0
+ *    1111 -> 0101 or 1010 return 2 (steps)
+ *
+ *
+ * thought:
+ *    iter string if cur pointing char is same flip it
+ *
+ */
+public class MiniChangesToMakeAlterBinaryString {
+
+    public static void main(String[] args) {
+        System.out.println(solution("0100"));
+    }
+
+
+    public static int solution(String str ){
+
+        int result =0;
+
+        if (str.length()<=1)
+            return 0;
+
+        for (int i =0 ; i <str.length();i++){
+            if (  str.charAt(i)-'0' != i%2){
+                result++;
+            }
+        }
+
+
+
+        return Math.min(result,str.length()-result);
+    }
+}
